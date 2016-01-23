c363630912df02faf01c716d37029e63336db05c
ElliotWhiley <elliot.w@hotmail.com>
2016-01-23 16:30:20 +1300

    Added AsteroidSpawner prefab - asteroids have slight angular velocity and scaled up to 300% size

--- Assets/AsteroidSpawner.cs
+++ Assets/AsteroidSpawner.cs
@@ -2,6 +2,7 @@
 
 public class AsteroidSpawner : MonoBehaviour {
 
+    // Set variable values in AsteroidSpawner prefab in Unity scene
     public int numAsteroids;
     public int numAsteroidTypes;
     public GameObject[] asteroids;
@@ -22,9 +23,9 @@ public class AsteroidSpawner : MonoBehaviour {
             int asteroidNumber = Random.Range(0, numAsteroidTypes);
 
             // Generate random X/Y/Z spawn location
-            int posX = Random.Range(0, maxX);
-            int posY = Random.Range(0, maxY);
-            int posZ = Random.Range(0, maxZ);
+            int posX = Random.Range(0, maxX) - 50;
+            int posY = Random.Range(0, maxY) - 50;
+            int posZ = Random.Range(0, maxZ) - 50;
 
             // Generate random X/Y/Z spawn rotation
             int rotX = Random.Range(0, 360);
@@ -33,10 +34,16 @@ public class AsteroidSpawner : MonoBehaviour {
 
             // Instantiate that asteroid in a random position
             GameObject asteroid = (GameObject)Instantiate(asteroids[asteroidNumber], new Vector3(posX, posY, posZ), Quaternion.Euler(rotX, rotY, rotZ));
-            
+
+            // Increase scale of asteroid (1/1/1 => 3/3/3)
+            asteroid.transform.localScale += new Vector3(2, 2, 2);
+
             // Give asteroid random velocity (between min and max velocity) in the direction it is facing
-            int velocity = Random.Range(minVelocity, maxVelocity);
-            asteroid.GetComponent<Rigidbody>().velocity = Vector3.forward * velocity;
+            float velocity = Random.Range(minVelocity, maxVelocity);
+            asteroid.GetComponent<Rigidbody>().velocity = new Vector3(posX, posY, posZ) * (velocity / 5);
+
+            // Give asteroids a slight random rotation
+            asteroid.GetComponent<Rigidbody>().angularVelocity = new Vector3(posX, posY, posZ) * (velocity / 50);
         }
     }
 }
