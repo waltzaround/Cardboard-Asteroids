cc7c4eced3fb701341ae80ecb47ba36a2c522df9
Michael Andrew <michael@gladeye.co.nz>
2016-01-24 08:46:33 +1300

    Tidied up thrust script
    Also exposed speed variable

--- Assets/Scripts/Thrust.cs
+++ Assets/Scripts/Thrust.cs
@@ -3,10 +3,9 @@ using System.Collections;
 
 public class Thrust : MonoBehaviour {
     private bool thrustOn = false;
-    public float sensitivity = 0.25f;       // keep it from 0..1
+    public float speed = 0.25f;       // keep it from 0..1
     private Rigidbody rb;
-   // public GameObject[] playerheads;
-    public GameObject playerhead;
+    public GameObject playerHead;
     private Transform trans;
     private Vector3 lookVector;
 	private Vector3 forceVector;
@@ -16,30 +15,13 @@ public class Thrust : MonoBehaviour {
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-
-        //playerhead = GameObject.Find("Head");
-
-   //     if (playerhead == null)
-     //       playerheads = GameObject.FindGameObjectsWithTag("PlayerHead");
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-
-        //        trans = playerheads[0].GetComponent("Transform");
-
-        //      Vector3 lookVector = trans.eulerAngles;
-
-        trans = playerhead.GetComponent<Transform>();
+		trans = playerHead.GetComponent<Transform>();
         lookVector = trans.eulerAngles;
-        
-
-        //Vector3 dir = new Vector3();			// create (0,0,0)
-
-
-
-
 
 		if (Cardboard.SDK.Triggered)
         {
@@ -51,23 +33,15 @@ public class Thrust : MonoBehaviour {
             else {
                 thrustOn = false;
             }
-
-            //something here;   
         }
 
         if (thrustOn)
         {
-            //     dir.z += 0.05f;
-            //  rb.AddForce
             forceVector = Vector3.Scale(trans.forward, lookVector);
-			//relative force adds force to the object in local space without taking into consideration the world around it
-			//rb.AddRelativeForce(forceVector * 0.005f);
-
 
 			//add an instant velocity change along the current look direction
-			//rb.AddForce(trans.forward * 0.05f, ForceMode.VelocityChange);
-
-			rb.AddForce(trans.forward * 0.1f, ForceMode.VelocityChange);
+			//will be affected by drag of object this script is attached to
+			rb.AddForce(trans.forward * speed, ForceMode.VelocityChange);
         }
 
     }
