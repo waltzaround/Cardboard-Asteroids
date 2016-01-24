fd5453a98f1cbfbd77e668df675927afe2d3763b
ElliotWhiley <elliot.w@hotmail.com>
2016-01-24 19:42:01 +1300

    Detect when asteroid collides with player mesh boundary

--- Assets/Scripts/OnCollision.cs
+++ Assets/Scripts/OnCollision.cs
@@ -2,8 +2,14 @@
 
 public class OnCollision : MonoBehaviour {
 
-	// Detect collision
-	void OnCollisionEnter(Collision collision) {
-        Debug.Log("BOOOOOOM!");
-	}
+    public Spawner spawner;
+
+    // Detect collision
+    //void OnCollisionExit(Collision collision) {
+    //    if (collision.gameObject.name == "Player") {
+    //        spawner.RespawnAsteroid(this.gameObject);
+    //        //this.collider.name == ()
+    //    }
+    //    //Debug.Log(this.GetComponent<Collider>().name);
+    //}
 }
